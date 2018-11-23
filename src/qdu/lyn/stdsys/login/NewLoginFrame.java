51719435d34f42f7a47885a874bd157f9e34e09c
suvvm <915269675@qq.com>
2018-11-23 22:04:08 +0800

    package login add class newLoginFrame and add some images

--- src/qdu/lyn/stdsys/login/NewLoginFrame.java
+++ src/qdu/lyn/stdsys/login/NewLoginFrame.java
@@ -1,5 +1,15 @@
 package qdu.lyn.stdsys.login;
 
-public class NewLoginFrame {
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+public class NewLoginFrame extends JFrame{
+	public NewLoginFrame() {
+		setSize(new Dimension(1280, 768));
+		setTitle("StudentManager");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(NewLoginFrame.class.getResource("/ico/ico.png")));
+		getContentPane().setLayout(null);
+	}
 
 }
