725fd3b4fae6400b5934dd67bab9c0e6f4a21ddf
suvvm <915269675@qq.com>
2018-11-26 00:38:51 +0800

    create new package user create new class UserInf

---
+++ src/qdu/lyn/stdsys/user/UserInf.java
@@ -0,0 +1,19 @@
+package qdu.lyn.stdsys.user;
+
+public class UserInf {
+	private String userName;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String tUserName) {
+		userName = tUserName;
+	}
+	public String getUserPassword() {
+		return password;
+	}
+	public void setUserPassword(String tPassword) {
+		password = tPassword;
+	}
+}
