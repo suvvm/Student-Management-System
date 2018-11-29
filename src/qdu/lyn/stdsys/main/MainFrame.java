aaa2d20326bfdfd2553a18d6223f6df75b4f956b
suvvm <915269675@qq.com>
2018-11-29 11:56:10 +0800

    update MainFrame

--- src/qdu/lyn/stdsys/main/MainFrame.java
+++ src/qdu/lyn/stdsys/main/MainFrame.java
@@ -1,5 +1,39 @@
 package qdu.lyn.stdsys.main;
 
-public class MainFrame {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class MainFrame extends JFrame{
+	private JPanel contentPane;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO 自动生成的方法存根
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}	
+	public MainFrame() {
+		setTitle("主界面");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1280, 768);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 }
