af1f4eeba59ed9e821b615d2a719ed60ab6f319a
suvvm <915269675@qq.com>
2018-11-30 13:33:28 +0800

    update class Administrator add email

--- src/qdu/lyn/stdsys/user/Administrator.java
+++ src/qdu/lyn/stdsys/user/Administrator.java
@@ -4,7 +4,13 @@ public class Administrator {
 	private String userName;
 	private String password;
 	private String name;
-	
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String tEmail) {
+		email = tEmail;
+	}
 	public String getUserName() {
 		return userName;
 	}
@@ -19,5 +25,8 @@ public class Administrator {
 	}
 	public String getName() {
 		return name;
+	}
+	public void setName(String tName) {
+		name = tName;
 	}
 }
