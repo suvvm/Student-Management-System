3698a77ee80b20abc15b43814c4767e5c5ae571d
suvvm <915269675@qq.com>
2018-12-01 21:24:45 +0800

    class Student add name and email

--- src/qdu/lyn/stdsys/user/Student.java
+++ src/qdu/lyn/stdsys/user/Student.java
@@ -3,7 +3,20 @@ package qdu.lyn.stdsys.user;
 public class Student{
 	private String userName;
 	private String password;
-	
+	private String name;
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String tEmail) {
+		email = tEmail;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String tName) {
+		name = tName;
+	}
 	public String getUserName() {
 		return userName;
 	}
