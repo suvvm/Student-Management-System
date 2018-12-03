811337e22dde47e930dc462617c8c89c3399128c
suvvm <915269675@qq.com>
2018-12-04 01:48:54 +0800

    package user add new class StudentInf.java

---
+++ src/qdu/lyn/stdsys/user/StudentInf.java
@@ -0,0 +1,67 @@
+package qdu.lyn.stdsys.user;
+
+public class StudentInf {
+	private int id;
+	private String name;
+	private String className;
+	private String home;
+	private String phoneNumber;
+	private String qqNumber;
+	private String fileUrl;
+	private String email;
+	private String userName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getQqNumber() {
+		return qqNumber;
+	}
+	public void setQqNumber(String qqNumber) {
+		this.qqNumber = qqNumber;
+	}
+	public String getFileUrl() {
+		return fileUrl;
+	}
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
