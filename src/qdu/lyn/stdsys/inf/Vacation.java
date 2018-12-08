fd1a953d7b3c2f74205674d8c7b84848881c0784
suvvm <915269675@qq.com>
2018-12-08 18:47:07 +0800

    create new package inf and add new class Vacation to inf

---
+++ src/qdu/lyn/stdsys/inf/Vacation.java
@@ -0,0 +1,47 @@
+package qdu.lyn.stdsys.inf;
+
+public class Vacation {
+	private int stuid;
+	private String stuName;
+	private String bgTime;
+	private String edTime;
+	private String reason;
+	private String vacDestination;
+	public int getStuid() {
+		return stuid;
+	}
+	public void setStuid(int stuid) {
+		this.stuid = stuid;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	public String getBgTime() {
+		return bgTime;
+	}
+	public void setBgTime(String bgTime) {
+		this.bgTime = bgTime;
+	}
+	public String getEdTime() {
+		return edTime;
+	}
+	public void setEdTime(String edTime) {
+		this.edTime = edTime;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getVacDestination() {
+		return vacDestination;
+	}
+	public void setVacDestination(String vacDestination) {
+		this.vacDestination = vacDestination;
+	}
+	
+}
