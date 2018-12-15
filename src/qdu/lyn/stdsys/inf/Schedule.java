c29ae26169c1d32cce6af66b38f01a2bc97300de
suvvm <915269675@qq.com>
2018-12-15 21:59:41 +0800

    create new class Schedule

---
+++ src/qdu/lyn/stdsys/inf/Schedule.java
@@ -0,0 +1,332 @@
+package qdu.lyn.stdsys.inf;
+
+
+public class Schedule {
+	
+	public class Monday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Tuesday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Wednesday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Thursday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Friday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Staurday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+		
+	}
+	public class Sunday{
+		private String course_12;
+		private String course_34;
+		private String course_56;
+		private String course_78;
+		private String course_910;
+		public String getCourse_12() {
+			return course_12;
+		}
+		public void setCourse_12(String course_12) {
+			this.course_12 = course_12;
+		}
+		public String getCourse_34() {
+			return course_34;
+		}
+		public void setCourse_34(String course_34) {
+			this.course_34 = course_34;
+		}
+		public String getCourse_56() {
+			return course_56;
+		}
+		public void setCourse_56(String course_56) {
+			this.course_56 = course_56;
+		}
+		public String getCourse_78() {
+			return course_78;
+		}
+		public void setCourse_78(String course_78) {
+			this.course_78 = course_78;
+		}
+		public String getCourse_910() {
+			return course_910;
+		}
+		public void setCourse_910(String course_910) {
+			this.course_910 = course_910;
+		}
+	}
+	
+	private Monday monday;
+	private Tuesday tuesday;
+	private Wednesday wednesday;
+	private Thursday thursday;
+	private Friday friday;
+	private Staurday staurday;
+	private Sunday sunday;
+	public Schedule() {
+		// TODO 自动生成的构造函数存根
+		monday = new Monday();
+		tuesday = new Tuesday();
+		wednesday = new Wednesday();
+		thursday = new Thursday();
+		friday = new Friday();
+		staurday = new Staurday();
+		sunday = new Sunday();
+		
+	}
+	public Monday getMonday() {
+		return monday;
+	}
+	public void setMonday(Monday monday) {
+		this.monday = monday;
+	}
+	public Tuesday getTuesday() {
+		return tuesday;
+	}
+	public void setTuesday(Tuesday tuesday) {
+		this.tuesday = tuesday;
+	}
+	public Wednesday getWednesday() {
+		return wednesday;
+	}
+	public void setWednesday(Wednesday wednesday) {
+		this.wednesday = wednesday;
+	}
+	public Thursday getThursday() {
+		return thursday;
+	}
+	public void setThursday(Thursday thursday) {
+		this.thursday = thursday;
+	}
+	public Friday getFriday() {
+		return friday;
+	}
+	public void setFriday(Friday friday) {
+		this.friday = friday;
+	}
+	public Staurday getStaurday() {
+		return staurday;
+	}
+	public void setStaurday(Staurday staurday) {
+		this.staurday = staurday;
+	}
+	public Sunday getSunday() {
+		return sunday;
+	}
+	public void setSunday(Sunday sunday) {
+		this.sunday = sunday;
+	}
+}
