29029496e491fb53cd398193fffabc7a045539a9
suvvm <915269675@qq.com>
2018-12-23 16:45:21 +0800

    create new class MesssagesInf

---
+++ src/qdu/lyn/stdsys/inf/MessagesInf.java
@@ -0,0 +1,40 @@
+package qdu.lyn.stdsys.inf;
+
+public class MessagesInf {
+	private String name;
+	private String isRead;
+	private String sendUser;
+	private String toUser;
+	private String content;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIsRead() {
+		return isRead;
+	}
+	public void setIsRead(String isRead) {
+		this.isRead = isRead;
+	}
+	public String getSendUser() {
+		return sendUser;
+	}
+	public void setSendUser(String sendUser) {
+		this.sendUser = sendUser;
+	}
+	public String getToUser() {
+		return toUser;
+	}
+	public void setToUser(String toUser) {
+		this.toUser = toUser;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
